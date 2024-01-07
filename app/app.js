18c94b8c07c1b0618556bae57b9429d6ca52b7b1
techvishal1988 <104866564+techvishal1988@users.noreply.github.com>
2024-01-07 13:19:07 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-app.get('/hello', (req, res) => res.send('Happy new years'))
+app.get('/hello', (req, res) => res.send('Happy new years 2k24'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 module.exports.handler = serverless(app)
